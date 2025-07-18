3ade6689c0c183febae0d33005c6cd6b05718032
Maximiliano Rebainera <maxi.reba@gmail.com>
2025-07-18 11:12:23 -0300

    login

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { ChocoDatosService } from '../choco-datos.service';
 
 @Component({
   selector: 'app-admin',
@@ -10,10 +11,18 @@ import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validatio
 })
 export class AdminComponent {
 
+@Output()
+  administrador:EventEmitter<boolean>= new EventEmitter<boolean>();
+  isAdmin: boolean = false;
+
+ constructor(private chocosService: ChocoDatosService) { 
+ }
+
+
   formularioAdmin = new FormGroup({
     nombre: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)]),
     repetirPassword: new FormControl('', [Validators.required]),
   }, [this.isMismatch]);
 
@@ -24,6 +33,15 @@ export class AdminComponent {
   }
 
   OnSubmit() {
-    console.log(this.formularioAdmin.value);
+    const { nombre, email, password } = this.formularioAdmin.value;
+    if (this.chocosService) {
+      this.chocosService.validarUsuario(nombre ?? '', email ?? '', password ?? '');
+    }
+    if(this.chocosService.validarUsuario(nombre ?? '', email ?? '', password ?? '')) {
+      this.isAdmin = true;
+      this.administrador.emit(this.isAdmin);
+    }
   }
+
 }
+
