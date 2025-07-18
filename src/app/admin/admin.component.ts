053caa7c24bf5cc8c21412b0da3619b76fb22e28
Maximiliano Rebainera <maxi.reba@gmail.com>
2025-07-18 11:12:23 -0300

    login

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { ChocoDatosService } from '../choco-datos.service';
 
 @Component({
   selector: 'app-admin',
@@ -10,10 +11,18 @@ import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validatio
 })
 export class AdminComponent {
 
+@Output()
+  administrador:EventEmitter<boolean>= new EventEmitter<boolean>();
+  isAdmin: boolean = false;
+
+ constructor(private chocosService: ChocoDatosService) { 
+ }
+
+
   formularioAdmin = new FormGroup({
     nombre: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)]),
     repetirPassword: new FormControl('', [Validators.required]),
   }, [this.isMismatch]);
 
@@ -24,6 +33,13 @@ export class AdminComponent {
   }
 
   OnSubmit() {
-    console.log(this.formularioAdmin.value);
+    const { nombre, email, password } = this.formularioAdmin.value;
+    let access = this.chocosService.validarUsuario(nombre ?? '', email ?? '', password ?? '');
+    if(access) {
+      this.isAdmin = true;
+      this.administrador.emit(this.isAdmin);
+    }
   }
+
 }
+
--- src/app/choco-datos.service.ts
+++ src/app/choco-datos.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { Chocolate } from './choco-list/Choco';
 
-const URL = 'https://68604e8b8e7486408442f0a7.mockapi.io/rauch-chocolates/Chocolates';
+const URL = 'https://68604e8b8e7486408442f0a7.mockapi.io/rauch-chocolates';
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +14,17 @@ export class ChocoDatosService {
 
   public getAll(): Observable<Chocolate[]> {
 
-        return this.http.get<Chocolate[]>(URL)
+        return this.http.get<Chocolate[]>(`${URL}/Chocolates`)
         .pipe(
           tap((chocolates) => chocolates.forEach(choco => choco.cantidad = 0)) // Inicializar cantidad a 0
         );
       }
-    }
-    
+
+      public validarUsuario(nombre: string, email: string, password: string): Observable<boolean> {
+        const body = { nombre, email, password };
+        return this.http.post<{ valido: boolean }>(`${URL}/usuarios`, body)
+          .pipe(
+            map(response => response.valido)
+          );
+      }
+}
